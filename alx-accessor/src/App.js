58e295fdbbf3d0f3b01b8aef3e62f45859b52959
Paul Wamaria <paulwamaria@gmail.com>
2020-10-13 09:13:03 +0300

    Initial file structure bootstrapped with create react app

---
+++ alx-accessor/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-success">Welcome to ALX-Accessor home page, </h1>
+      <h3 className="text-warning">The website is under construction</h3>
+    </div>
+  );
+}
+
+export default App;
