687552bae8680104158eec6df03acbca789a3bbf
Paul Wamaria <paulwamaria@gmail.com>
2020-10-14 12:15:39 +0300

    Add a reset password component

---
+++ alx-accessor/src/componets/auth/ResetPassword.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class ResetPassword extends Component {
+  render() {
+    return <div>Reset password page is being updated</div>;
+  }
+}
+
+export default ResetPassword;
