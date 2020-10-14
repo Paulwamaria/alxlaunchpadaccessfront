1a0050367e2b6d931690b14d32f04cf8977938a8
Paul Wamaria <paulwamaria@gmail.com>
2020-10-14 12:14:16 +0300

    Add the login component

---
+++ alx-accessor/src/componets/auth/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+export class Login extends Component {
+    render() {
+        return (
+            <div>
+                The login page is being updated
+            </div>
+        )
+    }
+}
+
+export default Login
