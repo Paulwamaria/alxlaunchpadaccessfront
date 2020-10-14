28b992ffc7bfa158216029c95178b64fef59971b
Paul Wamaria <paulwamaria@gmail.com>
2020-10-14 12:41:11 +0300

    Add a footer

--- alx-accessor/src/componets/layout/Footer.js
+++ alx-accessor/src/componets/layout/Footer.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { MDBContainer, MDBFooter } from "mdbreact";
 
 export class Footer extends Component {
   render() {
-    return <div>The footer is being updated</div>;
+    return (
+      <MDBFooter className="#1c2a48 mdb-color darken-3 font-small pt-4 mt-4 fixed-bottom">
+        <div className="footer-copyright text-center py-3">
+          <MDBContainer fluid>
+            &copy; {new Date().getFullYear()} Copyright:{" "}
+            <Link to="/licence">alx</Link>
+          </MDBContainer>
+        </div>
+      </MDBFooter>
+    );
   }
 }
 
