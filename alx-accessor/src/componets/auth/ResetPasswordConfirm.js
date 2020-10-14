075d8c564637917d8e552389ab60449c33ad401b
Paul Wamaria <paulwamaria@gmail.com>
2020-10-14 12:16:04 +0300

    Add a reset password confirmation component

---
+++ alx-accessor/src/componets/auth/ResetPasswordConfirm.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class ResetPasswordConfirm extends Component {
+  render() {
+    return <div>Reset password confirm page is being updated</div>;
+  }
+}
+
+export default ResetPasswordConfirm;
