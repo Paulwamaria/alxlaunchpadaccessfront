7d128a2f88c8f8d98347aca6e4b1baf083697080
Paul Wamaria <paulwamaria@gmail.com>
2020-10-14 23:29:06 +0300

    call the loaduser and check authentication status when the component mounts

--- alx-accessor/src/App.js
+++ alx-accessor/src/App.js
@@ -3,8 +3,19 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Mynavbar from "./componets/layout/MyNavbar";
 import Footer from "./componets/layout/Footer";
+import { connect } from "react-redux";
+import { load_user, checkAuthStatus } from "./actions/auth";
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  componentDidMount = () => {
+    this.props.loadUser();
+    this.props.checkAuthStatus();
+  };
   render() {
     return (
       <Router>
@@ -17,4 +28,17 @@ export class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.authReducer.isAuthenticated,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    checkAuthStatus: () => dispatch(checkAuthStatus()),
+    loadUser: () => dispatch(load_user()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
