b3a276b9992aec73dcf7c60aa3f0ddb83e9ea7e6
Paul Wamaria <paulwamaria@gmail.com>
2020-10-14 12:14:47 +0300

    Add the sign up component

---
+++ alx-accessor/src/componets/auth/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Register extends Component {
+    render() {
+        return (
+            <div>
+                The registration page is being updated
+            </div>
+        )
+    }
+}
+
+export default Register
