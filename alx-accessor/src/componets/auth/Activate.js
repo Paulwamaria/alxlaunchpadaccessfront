9d0364d1534ec5aabe3349b653b3edb03408f8ac
Paul Wamaria <paulwamaria@gmail.com>
2020-10-15 18:07:55 +0300

    Add activate account component

--- alx-accessor/src/componets/auth/Activate.js
+++ alx-accessor/src/componets/auth/Activate.js
@@ -1,9 +1,66 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { verify } from "../../actions/auth";
 
 export class Activate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      verified: false,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  verifyAccount = (event) => {
+    event.preventDefault();
+    const uid = this.props.match.params.uid;
+    const token = this.props.match.params.token;
+    this.props.verify(uid, token);
+    this.setState({
+      verified: true,
+    });
+  };
+
   render() {
-    return <div>The activation page is being updated</div>;
+    if (this.state.verified) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3"></div>
+          <div className="col-md-6">
+            <h2 className="mt-5">Verify Your Account</h2>
+            <div>
+              <button type="submit" onClick={this.props.verify}>
+                Verify
+              </button>
+            </div>
+          </div>
+          <div className="col-md-3"></div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Activate;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.authReducer.isAuthenticated,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    verify: () => dispatch(verify()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Activate);
