1292c99b0223c60a1c6cdf99f610ac65f9801d5e
Paul Wamaria <paulwamaria@gmail.com>
2020-10-15 18:09:43 +0300

    Add reset password component

--- alx-accessor/src/componets/auth/ResetPassword.js
+++ alx-accessor/src/componets/auth/ResetPassword.js
@@ -1,9 +1,83 @@
 import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { resetPassword, checkAuthStatus } from "../../actions/auth";
 
 export class ResetPassword extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      requestSent: false,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.resetPassword(this.state.email);
+    this.setState({
+      requestSent: true,
+    });
+  };
+
   render() {
-    return <div>Reset password page is being updated</div>;
+    if (this.state.requestSent) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3"></div>
+          <div className="col-md-6">
+            <form onSubmit={this.handleSubmit} className="mt-5 mb-5 ">
+              <legend className="text">Request Password Reset</legend>
+              <div>
+                <label htmlFor="email">Email</label>
+              </div>
+              <div>
+                <input
+                  className="form-control"
+                  type="text"
+                  id="email"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </div>
+
+              <button className="mt-3" type="submit">
+                Reset Password
+              </button>
+              <p className="mt-2">
+                Go back to login? <Link to="/login">Login</Link>
+              </p>
+            </form>
+          </div>
+          <div className="col-md-3"></div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default ResetPassword;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.authReducer.isAuthenticated,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    resetPassword: (email) => dispatch(resetPassword(email)),
+    checkAuthStatus: () => dispatch(checkAuthStatus()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ResetPassword);
