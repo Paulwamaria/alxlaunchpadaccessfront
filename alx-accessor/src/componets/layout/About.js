2991943b2b90f185bb1e6cfc387da8b737f3482f
Paul Wamaria <paulwamaria@gmail.com>
2020-10-17 11:01:00 +0300

    Add about content

--- alx-accessor/src/componets/layout/About.js
+++ alx-accessor/src/componets/layout/About.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class About extends Component {
   render() {
-    return <div>This website is being updated</div>;
+    return (
+      <div classsName="container-fluid">
+        <h5 className="mt-5">About Alx-accessor</h5>
+        <p>
+          A platform which aims to support Young leaders to access ALX Launchpad
+          services including to explore content, discover opportunities &
+          events, and engage with peers, facilitators, or other users. However,
+          the website is still under construction and the information available
+          is quality jokes from the official joke API and quality animations
+          from Kitsu API. Join our community fo find out more.
+        </p>
+        <Link to="/register">Join Alx-accessor</Link>
+      </div>
+    );
   }
 }
 
