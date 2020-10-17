41c14201fd141f9c03890403a23a9622a10765e0
Paul Wamaria <paulwamaria@gmail.com>
2020-10-17 11:01:27 +0300

    Add Anim action function

---
+++ alx-accessor/src/actions/anim.js
@@ -0,0 +1,29 @@
+import { GET_ANIME_SUCCESS, GET_ANIME_FAIL } from "../types/Types";
+import axios from "axios";
+
+// GET ANIME ACTION
+export const getAnime = (category) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/vnd.api+json",
+      Authorization: `Bearer ${process.env.REACT_APP_KITSU_TOKEN}`,
+    },
+  };
+
+  try {
+    const response = await axios.get(
+      `${process.env.REACT_APP_KITSU_URL}/anime?filter[categories]=${category}`,
+      config
+    );
+    dispatch({
+      type: GET_ANIME_SUCCESS,
+      payload: response.data.data,
+    });
+  } catch (err) {
+    console.log(err);
+    dispatch({
+      type: GET_ANIME_FAIL,
+    });
+  }
+};
