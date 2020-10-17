51163adfbdda505f3838d8c29212665dcffd21d9
Paul Wamaria <paulwamaria@gmail.com>
2020-10-17 11:03:10 +0300

    Add anime reducer to the rootReducer

--- alx-accessor/src/reducers/index.js
+++ alx-accessor/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth";
 import jokeReducer from "./jokeReducer";
+import animReducer from "./kitsuReducer";
 
 const rootReducer = combineReducers({
   authReducer,
   jokeReducer,
+  animReducer,
 });
 
 export default rootReducer;
